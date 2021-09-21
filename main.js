5c03b82163e6e9fe0d2b92309e2ad5e7cd23efe3
zThundy <antonioantonelli7@gmail.com>
2021-09-21 10:36:44 +0200

    changed db error handling

--- main.js
+++ main.js
@@ -7,32 +7,32 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.login(config.token);
 
-const con = mysql.createConnection({
+var con = mysql.createConnection({
     host: config.database.host,
     user: config.database.user,
     database: config.database.database,
     password: config.database.password
-})
+});
 
 con.connect(err => {
     if (err) console.log(err);
     console.log("Connessione al database stabilita!");
-})
-
-setInterval(keepAlive, 600000);
-function keepAlive() {
-    con.query('SELECT 1');
-    console.log("Fired Keep-Alive");
-    return;
-}
+});
 
 con.on("error", err => {
     if (err) console.log(err);
+    con = mysql.createConnection({
+        host: config.database.host,
+        user: config.database.user,
+        database: config.database.database,
+        password: config.database.password
+    });
+    global.con = con;
 })
 
 global.con = con;
 
 client.on("ready", () => {
-    console.log("sono pronto e ascolto le tue cazzate");
+    console.log("Bot ready :)");
     init(client, config)
 });
