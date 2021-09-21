e8098e4f101028e44f71d8f9a6266914a6eac12c
zThundy <antonioantonelli7@gmail.com>
2021-09-21 10:37:33 +0200

    changes to init module

--- commands/init.js
+++ commands/init.js
@@ -4,92 +4,73 @@ import { MessageEmbed } from "discord.js"
 let paths = [];
 let timeouts = {};
 
-export function GetCommands() {
-    return paths;
-}
+export function GetCommands() { return paths; }
 
 export function RemoveFromPath(path) {
     paths = paths.filter(filterPath => filterPath.path != path)
 }
 
-export function GetCommand(command) {
-    return paths.filter(path => path.module == command)
-    // if (paths.find(command)) {
-    //     return paths.find(command).path
-    // }
-    // return null
-}
+export function GetCommand(command) { return paths.filter(path => path.module == command) }
 
 export async function RegisterCommand(client, path, file) {
     if (!file.endsWith(".js") || file.startsWith("init")) return;
-
     let props = await import(path);
     paths.push({ path: path, module: file.split(".")[0] });
-    // console.log(paths)
-
     console.info("\x1b[33m[Info] Comando caricato: " + file + "\x1b[0m")
     client.commands.set(file.split(".")[0], props);
-    // console.log(client.commands)
-
     if (props.init) props.init(client);
 }
 
 export async function init(client, config) {
     fs.readdir("./commands/", async (err, files) => {
         if (err) throw(err);
-
         files.forEach(async file => {
-            let status = fs.statSync("./commands/" + file)
+            let status = fs.statSync("./commands/" + file);
             if (status.isDirectory()) {
                 fs.readdir("./commands/" + file, async (err, subFiles) => {
                     if (err) throw(err);
-
                     subFiles.forEach(async subFile => {
-                        let path = "./" + file + "/" + subFile
-                        RegisterCommand(client, path, subFile)
-                    })
-                })
+                        let path = "./" + file + "/" + subFile;
+                        RegisterCommand(client, path, subFile);
+                    });
+                });
             } else {
-                let path = "./" + file
-                RegisterCommand(client, path, file)
+                let path = "./" + file;
+                RegisterCommand(client, path, file);
             }
         });
     });
 
     client.on("message", (message) => {
         if (message.author.bot) return;
-        let args = message.content.split(" ")
+        let args = message.content.split(" ");
         if (args[0].indexOf(config.prefix) == -1) return;
-
         try {
-            let command = args[0].replace(config.prefix, "").toLowerCase()
-            let prop = client.commands.get(command)
-
+            let command = args[0].replace(config.prefix, "").toLowerCase();
+            let prop = client.commands.get(command);
             if (prop && prop.run) {
                 // timeout check per guild
                 if (!config.admins.includes(message.author.id)) {
                     if (timeouts[message.guild.id]) {
                         let embed = new MessageEmbed()
                             .setDescription("Give me some time to think 😧")
-                            .setColor("#FFFF00")
-                        message.channel.send({ embed })
-                        return
+                            .setColor("#FFFF00");
+                        message.channel.send({ embed });
+                        return;
                     }
-
                     // timeout check per guild
-                    if (!timeouts[message.guild.id]) timeouts[message.guild.id] = true
+                    if (!timeouts[message.guild.id]) timeouts[message.guild.id] = true;
                     setTimeout(() => {
-                        delete timeouts[message.guild.id]
-                    }, config.musicPlayer.timeBetweenCommands * 1000)
+                        delete timeouts[message.guild.id];
+                    }, config.musicPlayer.timeBetweenCommands * 1000);
                 }
-
                 // main funzion trigger
-                prop.run(client, args, message)
+                prop.run(client, args, message);
             } else {
                 let embed = new MessageEmbed()
                     .setDescription("This command does not exists 😵")
-                    .setColor("#FF0000")
-                message.channel.send({ embed })
+                    .setColor("#FF0000");
+                message.channel.send({ embed });
             }
         } catch (e) { console.error(e) }
     });
@@ -98,11 +79,8 @@ export async function init(client, config) {
         try {
             if (reactionMessage.me) return;
             paths.forEach(async file => {
-                // da cambiare assolutamente e da rendere modulare
-                // let command = file.path.match(/\/(.*?).js/)[1]
                 let command = file.module;
                 let prop = client.commands.get(command);
-
                 if (prop && prop.reactionAdd) prop.reactionAdd(client, reactionMessage, user);
             });
         } catch (e) { console.error(e) }
@@ -111,10 +89,8 @@ export async function init(client, config) {
     client.on("guildMemberAdd", async (member) => {
         try {
             paths.forEach(async file => {
-                // let command = path.match(/\/(.*?).js/)[1]
                 let command = file.module;
                 let prop = client.commands.get(command);
-
                 if (prop && prop.memberJoin) prop.memberJoin(client, member);
             });
         } catch (e) { console.error(e) }
@@ -123,19 +99,22 @@ export async function init(client, config) {
     client.on('voiceStateUpdate', (oldState, newState) => {
         try {
             paths.forEach(async file => {
-                // let command = path.match(/\/(.*?).js/)[1]
                 let command = file.module;
                 let prop = client.commands.get(command);
-
                 if (prop && prop.userJoinChannel) prop.userJoinChannel(client, oldState, newState);
             });
         } catch (e) { console.error(e) }
-    })
+    });
 
     client.on("guildCreate", (guild) => {
         global.con.query(`INSERT INTO servers(id) VALUES(${guild.id})`, (err, result) => {
             if (err) throw err;
-        })
+            paths.forEach(async file => {
+                let command = file.module;
+                let prop = client.commands.get(command);
+                if (prop && prop.botJoinedGuild) prop.botJoinedGuild(guild);
+            });
+        });
     });
 
     client.on("guildDelete", (guild) => {
@@ -144,10 +123,10 @@ export async function init(client, config) {
         })
     });
 
-    let status = `Online on ${client.guilds.cache.size} servers | ${config.prefix}help`
+    let status = `Online on ${client.guilds.cache.size} servers | ${config.prefix}help`;
     if (client.guilds.cache.size == 1) { status = `Online on ${client.guilds.cache.size} server | ${config.prefix}help` }
 
-    client.user.setPresence({ activity: { name: status }, status: 'dnd' })
+    client.user.setPresence({ activity: { name: status }, status: 'dnd' });
 
     // if (config.status == "dev") return;
     // client.user.setAvatar(config.avatar)
