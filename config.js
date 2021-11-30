ceeaf77cdd352791790258fa840d9a4dccbb6f4d
zThundy <antonioantonelli7@gmail.com>
2021-11-30 22:01:59 +0100

    added sqlite3 integration and removed a lot of code

--- config.js
+++ config.js
@@ -5,106 +5,14 @@ export default {
         clientId: "YOUR_SPOTIFY_ID_HERE",
         secret: "YOUR_SPOTIFY_SECRET_HERE"
     },
-    warn: {
-        role1: {
-            id: "800393386250076171",
-            assignOn: 1
-        },
-        role2: {
-            id: "800393411722477608",
-            assignOn: 4
-        },
-        role3: {
-            id: "800393437483630592",
-            assignOn: 6
-        },
-        maxView: 5
     },
     musicPlayer: {
         defaultVolume: 0.5,
-        timeBetweenCommands: 7, // in seconds
+        timeBetweenCommands: 3, // in seconds
         queueMaxView: 5,
     },
-    currency: {
-        enableDefaultCurrency: true,
-        defaultCurrency: 500,
-        trisBonus: 20,
-        emojis: {
-            // emoji: percentuale di vittoria
-            // showPerc multipli di 10 (compreso 0)
-            "💵": {
-                showPerc: 0,
-                winningPerc: 10
-            },
-            "🍌": {
-                showPerc: 10,
-                winningPerc: 5
-            },
-            "🍉": {
-                showPerc: 20,
-                winningPerc: 4
-            },
-            "🍇": {
-                showPerc: 30,
-                winningPerc: 3.5
-            },
-            "🍓": {
-                showPerc: 40,
-                winningPerc: 3
-            },
-            "🍒": {
-                showPerc: 50,
-                winningPerc: 2.5
-            },
-            "🍏": {
-                showPerc: 60,
-                winningPerc: 2
-            },
-            "🥔": {
-                showPerc: 70,
-                winningPerc: 1.5
-            },
-            "🧄": {
-                showPerc: 80,
-                winningPerc: 1
-            },
-            "🧅": {
-                showPerc: 90,
-                winningPerc: 0.5
-            },
-            "🦴": {
-                showPerc: 100,
-                winningPerc: 0
-            }
-        }
-    },
     admins: ["341296805646041100"],
-    ticketRole: "800365783527063552",
     commands: [
-        {
-            command: "bet",
-            description: "This command will start the slot machine minigame 🎰",
-            args: [
-                {
-                    name: "daily",
-                    description: "Adding this argument will check if the daily roll is ready or not"
-                },
-                {
-                    name: "number",
-                    description: "Adding this argument will start the minigame by removing the typed amount from your balance"
-                },
-                {
-                    name: "setup",
-                    description: "Adding this argument will setup the current channel as the minigame only channel",
-                    onlyAdmins: true
-                },
-                {
-                    name: "give",
-                    description: "Adding this argument followed by 'me' or '@user' and a number will give the currency amount to the user",
-                    onlyAdmins: true
-                }
-            ]
-        },
         {
             command: "play",
             description: "Start a song stream in your voice channel via youtube search 🔊",
@@ -148,21 +56,6 @@ export default {
         {
             command: "nowplaying",
             description: "If a song is playing, will show the song general informations 📜",
-        },
-        {
-            command: "ticket",
-            description: "Setup a ticket system for your discord server 📚",
-            args: [
-                {
-                    name: "create",
-                    description: "Adding this argument the bot will register the current channel as the 'create ticket' channel",
-                    onlyAdmins: true
-                },
-                {
-                    name: "close",
-                    description: "Adding this argument in the correct channel, will let the bot close the current ticket"
-                }
-            ]
         }
     ]
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Discord from "discord.js";
 import Colors from "./colors.js";
 import { init } from "./commands/init.js";
 import config from "./config.js";
+import SQL from "./sqlite.js";
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -12,10 +13,11 @@ const colors = new Colors();
 
 client.on("ready", () => {
     console.log(colors.changeBackground("green", "Bot authed successfully :)"));
+    client.database = database;
     init(client, config);
 });
 
-client.on('shardError', (error) => {
+client.on('shardError', error => {
 	console.error(colors.changeBackground("red", "Error on connecting to discord: " + error));
 });
 
