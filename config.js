9a46ab97b4af3bf28c80788505ed97eed24655d4
zThundy <antonioantonelli7@gmail.com>
2021-12-20 22:13:17 +0100

    updated config file

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ export default {
     spotify: {
         clientId: "YOUR_SPOTIFY_ID_HERE",
         secret: "YOUR_SPOTIFY_SECRET_HERE"
+    lyrics: {
+        token: "YOUR_MUSIXMATCH_TOKEN_HERE"
     },
     musicPlayer: {
         defaultVolume: 0.5,
@@ -11,50 +13,4 @@ export default {
         queueMaxView: 5,
     },
     admins: ["341296805646041100"],
-    commands: [
-        {
-            command: "play",
-            description: "Start a song stream in your voice channel via youtube search 🔊",
-            args: [
-                {
-                    name: "youtube-link",
-                    description: "Adding this argument the bot will join (if not already) to your channel and will start the song playback of the given youtube link"
-                },
-                {
-                    name: "video title",
-                    description: "Adding this argument the bot will join (if not already) to your channel and will start the playback of the first result of a youtube search"
-                }
-            ]
-        },
-        {
-            command: "splay",
-            description: "Start a song stream in your voice channel via spotify search 🔊",
-            args: [
-                {
-                    name: "spotify-link",
-                    description: "Adding this argument the bot will join (if not already) to your channel and will start the song playback of the given spotify link"
-                },
-                {
-                    name: "spotify song title",
-                    description: "Adding this argument the bot will join (if not already) to your channel and will start the playback of the first result of a spotify search"
-                }
-            ]
-        },
-        {
-            command: "stop",
-            description: "Stop the current song stream and disconnects the bot 🔇",
-        },
-        {
-            command: "skip",
-            description: "Skip the current song to the next in queue (if present) ⏩",
-        },
-        {
-            command: "queue",
-            description: "Show the list of queued songs in this server 🔠",
-        },
-        {
-            command: "nowplaying",
-            description: "If a song is playing, will show the song general informations 📜",
-        }
-    ]
 }
