a585551e3342edeee250e8bf69ab712dc0ba2bd2
zThundy <antonioantonelli7@gmail.com>
2021-12-22 17:30:21 +0100

    fixed lyrics command

--- commands/yt-music/lyrics.js
+++ commands/yt-music/lyrics.js
@@ -20,7 +20,15 @@ export async function run(client, args, message) {
     }
 
     const lyrics = await client.lyrics.getTrackLyrics(song.title, song.author.name);
-    lyrics.lyrics_body = lyrics.lyrics_body.replace("** This Lyrics is NOT for Commercial use **", "** You can only view 30% of the lyric **")
+    lyrics.lyrics_body = lyrics.lyrics_body.split("...");
+    lyrics.lyrics_body = lyrics.lyrics_body[0];
+    // just randomly check if the string is longer than 100 because
+    // if not, it's an error UwU
+    if (lyrics.lyrics_body.length > 100) {
+        const link = await client.lyrics.getTrackLyricUrl(song.title, song.author.name)
+        lyrics.lyrics_body += "\n\n** You can only view 30% of the lyric **";
+        lyrics.lyrics_body += `\n**[MusixMatch Link](${link})**`;
+    }
     let embed = new MessageEmbed()
         .setDescription(lyrics.lyrics_body)
         .setColor("#6600CC")
