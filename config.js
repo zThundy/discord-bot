e853e57341f23dd248691eafc6632911ff958cc4
zThundy <antonioantonelli7@gmail.com>
2022-01-09 16:43:03 +0100

    done config fix

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 export default {
-    token: "YOUR_DISCORD_TOKEN_HERE",
     prefix: "!!",
 
     // token | Discord dev application token, https://discord.com/developers/applications
+    token: "YOUR_DISCORD_TOKEN_HERE",
 
     spotify: {
         // clientId | Client ID of your spotify application, https://developer.spotify.com/dashboard/applications
+        clientId: "YOUR_SPOTIFY_ID_HERE",
         // secret | The secret key of your spotify application, https://developer.spotify.com/dashboard/applications
+        secret: "YOUR_SPOTIFY_SECRET_HERE"
     },
 
     lyrics: {
         // token | Your musixmatch application token, https://developer.musixmatch.com/
+        token: "YOUR_MUSIXMATCH_TOKEN_HERE"
     },
 
     musicPlayer: {
@@ -21,6 +24,8 @@ export default {
 
     timeouts: {
         // timeBetweenCommands | in seconds
+        timeBetweenCommands: 3
     },
     
+    admins: ["YOUR_ADMIN_ID_HERE", "YOUR_OTHER_ADMIN_ID_HERE"]
 }
