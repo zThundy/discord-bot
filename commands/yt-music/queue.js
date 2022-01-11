038b725c0ecffb42581634244464696b614ad99e
zThundy <antonioantonelli7@gmail.com>
2022-01-11 16:24:25 +0100

    fixed queue message

--- commands/yt-music/queue.js
+++ commands/yt-music/queue.js
@@ -64,19 +64,11 @@ function _shiftQueue(client, reactionMessage, user) {
             queueIndex[message.guild.id]--;
         } else if (reactionMessage.emoji.name == "▶️") {
             if (queueIndex[message.guild.id] == songs.length) return;
-            console.log("(queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView)", (queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView))
-            console.log("queueIndex[message.guild.id]", queueIndex[message.guild.id])
-            console.log("client.config.musicPlayer.queueMaxView", client.config.musicPlayer.queueMaxView)
-            console.log("songs.length", songs.length)
-            if ((queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView) >= songs.length) return;
+            if ((queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView) > songs.length) return;
             queueIndex[message.guild.id]++;
         } else if (reactionMessage.emoji.name == "⏩") {
-            console.log("(queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView)", (queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView))
-            console.log("queueIndex[message.guild.id]", queueIndex[message.guild.id])
-            console.log("client.config.musicPlayer.queueMaxView", client.config.musicPlayer.queueMaxView)
-            console.log("songs.length", songs.length)
-            if ((queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView) >= songs.length) return;
-            queueIndex[message.guild.id] = songs.length - client.config.musicPlayer.queueMaxView;
+            if ((queueIndex[message.guild.id] + client.config.musicPlayer.queueMaxView) > songs.length) return;
+            queueIndex[message.guild.id] = songs.length - (client.config.musicPlayer.queueMaxView - 1);
             if (queueIndex[message.guild.id] < 0) queueIndex[message.guild.id] = 1;
         }
 
@@ -93,12 +85,9 @@ export async function reactionRemove(client, reactionMessage, user) { _shiftQueu
 
 const GetQueueDescription = (songs, minIndex, maxIndex) => {
     let description = "";
-
     for (var i in songs) {
         if ((Number(i) + 1) <= maxIndex && (Number(i) + 1) >= minIndex) {
-            var title = songs[i].title
-            if (title.length >= 40) title = title.slice(0, 40) + "...";
-
+            var title = (songs[i].title >= 40 ? songs[i].title.slice(0, 40) : songs[i].title) + "..."
             if (songs[i].allowRatings) {
                 description += `[${(Number(i) + 1)}] ${title} - ${FormatNumber(songs[i].likes)} 👍\n`;
             } else {
