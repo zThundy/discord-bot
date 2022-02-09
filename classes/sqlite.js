f5fdfd5e288a53fb844ca1a2f7baa6d760c490d4
zThundy <antonioantonelli7@gmail.com>
2022-02-09 20:44:33 +0100

    added name field to servers table

--- classes/sqlite.js
+++ classes/sqlite.js
@@ -15,7 +15,7 @@ class SQL {
     _init() {
         console.log(colors.changeColor("yellow", "Initializing SQLITE3 database"));
         this.db = new sqlite3.Database("./cache/database.db");
-        this.db.run("CREATE TABLE IF NOT EXISTS servers (id TEXT)");
+        this.db.run("CREATE TABLE IF NOT EXISTS servers (id TEXT, name TEXT)");
         this.db.run("CREATE TABLE IF NOT EXISTS songs (guild VARCHAR, id VARCHAR, data TEXT)");
     }
 
