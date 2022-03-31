9a3d27531e70daa35004479d4c3587e1acac9296
zThundy <antonioantonelli7@gmail.com>
2022-03-31 17:29:55 +0200

    added check in loop command

--- commands/yt-music/loop.js
+++ commands/yt-music/loop.js
@@ -8,11 +8,19 @@ export function getCommandInfo() {
 }
 
 export async function run(client, args, message) {
-    const [loop, nowplaying] = client.player.loop();
-    let string = `Now looping **${nowplaying.title}**`;
-    if (loop) string = `Stopped looping of **${nowplaying.title}**`;
-    let embed = new MessageEmbed()
-        .setDescription(string)
-        .setColor("#FFFF00");
-    message.channel.send({ embed });
+    try {
+        const [loop, nowplaying] = client.player.loop();
+        let string = `Now looping **${nowplaying.title}**`;
+        if (loop) string = `Stopped looping of **${nowplaying.title}**`;
+        let embed = new MessageEmbed()
+            .setDescription(string)
+            .setColor("#FFFF00");
+        message.channel.send({ embed });
+    } catch(e) {
+        console.error(e);
+        let embed = new MessageEmbed()
+            .setDescription("No song playing rn 😔")
+            .setColor("#FF0000");
+        message.channel.send({ embed });
+    }
 }
