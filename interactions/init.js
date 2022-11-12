d1dd1c7fbe20bb26570e9ae8a543a5cae92c8cdc
zThundy <antonioantonelli7@gmail.com>
2022-11-12 15:39:13 +0100

    started working on interactions

--- interactions/init.js
+++ interactions/init.js
@@ -1,4 +1,5 @@
-import { REST, Routes, Collection } from "discord.js";
+import Discord from "discord.js";
+import { SlashCommandBuilder, SlashCommandSubcommandBuilder } from '@discordjs/builders';
 
 import { GetCommands, GetCommand, UnregisterCommand, RegisterCommand } from "../commands/init.js";
 import Colors from "../classes/colors.js";
@@ -7,21 +8,18 @@ const colors = new Colors();
 class Interactions {
     constructor(client) {
         this.client = client;
-        this.interactions = new Collection();
-        this.REST = new REST({ version: '10' }).setToken(client.config.token);
-        this.Routes = Routes;
-        // this.client.on("interactionCreate", this.handleInteraction.bind(this));
+        this.interactions = new Discord.Collection();
+        this.client.on("interactionCreate", this.handleInteraction.bind(this));
 
         setTimeout(async () => {
             const commands = GetCommands();
             for (let i = 0; i < commands.length; i++) {
                 const commandPath = "../commands/" + commands[i].path.replace("./", "");
-                console.log(commandPath);
                 const command = await import(commandPath);
-                console.log(command);
+                if (!command.getCommandInfo) continue;
                 const info = command.getCommandInfo();
                 this.register(command, info.command, info.description);
-                console.log(colors.changeColor("green", "[Info] Interaction " + info.command + " loaded"));
+                console.log(colors.changeColor("green", "[Info] Interaction " + info.command + ".js loaded"));
             }
         }, 3000);
     }
@@ -30,28 +28,45 @@ class Interactions {
         if (!interaction.isCommand()) return;
         const command = this.interactions.get(interaction.commandName);
         if (!command) return;
-        command.execute(interaction);
+        const args = interaction.options._hoistedOptions;
+        
+        command.run(this.client, args ? args.map(arg => arg.value) : [], interaction);
     }
 
     async register(command, name, description) {
-        this.interactions.set(name, command);
-        await this.REST.put(
-            this.Routes.applicationGuildCommands(this.client.user.id, this.client.config.guild),
-            {
-                body: {
-                    name: name,
-                    description: description,
-                    options: [
-                        {
-                            name: "song",
-                            description: "The song you want to play",
-                            type: "STRING",
-                            required: true
+        const interaction = new SlashCommandBuilder()
+            .setName(name)
+            .setDescription(description);
+        if (command.args) {
+            for (let i = 0; i < command.args.length; i++) {
+                const arg = command.args[i];
+                if (arg.subcommand) {
+                    const subcommand = new SlashCommandSubcommandBuilder()
+                        .setName(arg.name)
+                        .setDescription(arg.description);
+                    if (arg.args) {
+                        for (let j = 0; j < arg.args.length; j++) {
+                            const subarg = arg.args[j];
+                            subcommand.addStringOption(option => option.setName(subarg.name).setDescription(subarg.description).setRequired(subarg.required));
                         }
-                    ]
+                    }
+                    interaction.addSubcommand(subcommand);
+                } else {
+                    interaction.addStringOption(option => option.setName(arg.name).setDescription(arg.description).setRequired(true));
                 }
-            },
-        );
+            }
+        }
+
+        this.client.guilds.cache.forEach(async (guild) => {
+            const commands = await guild.commands.fetch();
+            const existingCommand = commands.find(cmd => cmd.name === name);
+            if (existingCommand) {
+                await existingCommand.delete();
+            }
+            await guild.commands.create(interaction);
+        });
+
+        this.interactions.set(name, command);
     }
 }
 
